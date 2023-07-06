cfdee97f2928427fd5597899a6fa112155033f4d
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-07-06 17:08:40 +0200

    fatto layout

--- index.php
+++ index.php
@@ -10,7 +10,24 @@
 </head>
 <body>
     <div id="app">
-        
+        <h1 class="text-center my-5">ToDoList</h1>
+        <div class="containers">
+            <div class="row">
+                <div class="col-4 offset-4">
+                    <ul class="list-group">
+                        <!-- LISTA -->
+                        <!-- -->
+                        <li v-for="(item, index) in array" :key="index" class="list-group-item d-flex align-items-center" :class="!item.done ? '' : 'text-decoration-line-through'">
+                            <span class="col">{{ item.text }}</span>
+                            <div class="col-auto functions-buttons d-flex">
+                                <div class="btn me-3" :class="!item.done ? 'btn-outline-success' : 'btn-outline-danger'" @click="doneUndoneTask(index)"><i :class="!item.done ? 'fa-solid fa-check' : 'fa-solid fa-xmark'"></i></div>
+                                <div class="btn btn-outline-warning"><i class="fa-solid fa-trash"></i></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 <!-- SCRIPTS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
--- script/script.js
+++ script/script.js
@@ -7,5 +7,17 @@ createApp({
 
         }
     },
-
+    mounted() {
+        axios.get('./server.php').then((resp) => {
+            this.array = resp.data;
+            console.log(this.array)
+        })
+    },
+    methods: {
+        doneUndoneTask(index){
+            this.array[index].done = !this.array[index].done;
+        },
+        
+        
+    },
 }).mount('#app')
